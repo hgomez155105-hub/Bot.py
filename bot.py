9f569bfcf9ac5a64f3e2b7fa89f4ed0f93294fa6
hgomez155105-hub <hgomez155105@gmail.com>
2026-01-13 10:50:03 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -204,8 +204,7 @@ if 'saldo_demo' not in st.session_state:
 # ============================================================
 
 c_h1, c_h2 = st.columns([4, 1])
-c_h1.markdown(f"## 👁️ H y G Inovaciones - "f"<span class='user-tag'>👤 {st.session_state_get('user_name, invitado')}"
-    unsafe_allow_html=True)
+c_h1.markdown(f"## 👁️ H y G Inovaciones - <span class='user-tag'>👤 {st.session_state.get('user_name, invitado')}" </span>", unsafe_allow_html=True)
 c_h2.image(LOGO_URL, width=70)
 
 # ============================================================
