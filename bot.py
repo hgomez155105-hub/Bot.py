0b84093a92803d239fe5331ee3b4ee2a46e51633
hgomez155105-hub <hgomez155105@gmail.com>
2026-01-14 18:36:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -282,12 +282,12 @@ else:
                 delta_color="off"
             )
             try:
-    balance = exchange.fetch_balance()
-    usdt_balance = balance['total']['USDT']
-    c2.metric("💰 Wallet REAL", f"${usdt_balance:,.2f}", delta=None)
-except Exception as e:
-    c2.metric("💰 Wallet REAL", "Error", delta=None)
-    st.warning("No se pudo obtener el saldo REAL. Verificá tus claves o conexión.")
+    			balance = exchange.fetch_balance()
+    			usdt_balance = balance['total']['USDT']
+    			c2.metric("💰 Wallet REAL", f"${usdt_balance:,.2f}", delta=None)
+			except Exception as e:
+    			c2.metric("💰 Wallet REAL", "Error", delta=None)
+    			st.warning("No se pudo obtener el saldo REAL. Verificá tus claves o conexión.")
             
             c3.metric(
                 "PNL Total",
